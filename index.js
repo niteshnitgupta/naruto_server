0dd1bd47e4222bf9180fc744e9f64343ddbadafc
unknown <NIGUPTA@BLRD4926.iis.amadeus.net>
2016-09-27 10:34:50 +0530

    Connection with MongoDB and insert new jutsu.

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 var express = require('express');
+var mongodb = require('mongodb');
+var datetime = require('node-datetime');
+var log4js = require('log4js');
+var justu = require('./jutsus/justu');
+
+var log = log4js.getLogger();
+
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/naruto';
+
 var app = express();
 
+
+
+
 app.get('/saveJutsu', function (req, res) {
-   res.send('Hello World');
+	MongoClient.connect(url, function (err, db) {
+		if (err) {
+			log.fatal('Unable to connect to database');
+		} else {
+			justu.addJutsu("J100", "Jutsu Sharingan", db);
+		}
+	});
+	res.send();
 });
 
 app.get('/getJutsu', function (req, res) {
-   res.send('Hello World');
+
 });
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+	res.send('Hello World');
 });
 
 var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+	var host = server.address().address
+	var port = server.address().port
 })
--- jutsus/justu.js
+++ jutsus/justu.js
@@ -1,8 +1,8 @@
+var datetime = require('node-datetime');
+var log4js = require('log4js');
+var log = log4js.getLogger();
+
 function GetJutsusNearMe() {
-
-}
-
-function AddJutsu() {
 
 }
 
@@ -15,5 +15,33 @@ function JutsuInVisible() {
 }
 
 function GetJutsuDetails() {
-	
+
+}
+
+exports.addJutsu = function (jutsu_name, description, db) {
+	var jutsu = {jutsu_name: jutsu_name, description: description};	
+	var collection = db.collection('jutsu_list');
+	collection.insert([jutsu], function (err, result) {
+		if (err) {
+			log.fatal('Unable to insert Jutsu');
+		} else {
+			jutsu_id = result.ops[0]._id;
+			addJutsuLog(jutsu_id, jutsu_name, "New Jutsu Added", db)
+			log.info('Jutsu added successfully');
+		}
+	});
+}
+
+var addJutsuLog = function (jutsu_id, jutsu_name, logs, db) {
+	var currentTimeStamp = datetime.create().format('d/m/Y H:M:S');
+	var jutsuLog = {jutsu_id:jutsu_id, jutsu_name: jutsu_name, timestamp: currentTimeStamp, log: logs};	
+	var collection = db.collection('jutsu_activity_logs');
+	collection.insert([jutsuLog], function (err, result) {
+		if (err) {
+			log.error(err);
+		} else {
+			log.info('Jutsu log added successfully');
+		}
+		db.close();
+	});
 }
