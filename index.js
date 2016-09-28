3d828441f7ea298803657632889d7bb962105583
unknown <NIGUPTA@BLRD4926.iis.amadeus.net>
2016-09-28 16:10:42 +0530

    socket io example

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
+/*var express = require('express');
+var http = require('http').Server(app);
 var mongodb = require('mongodb');
 var datetime = require('node-datetime');
 var log4js = require('log4js');
 var jutsu = require('./jutsus/jutsu');
+
 
 var log = log4js.getLogger();
 
@@ -10,6 +12,8 @@ var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:27017/naruto';
 
 var app = express();
+
+app.use(express.static(__dirname));
 
 
 app.get('/getJutsuDetails', function (req, res) {
@@ -41,11 +45,37 @@ app.get('/getJutsu', function (req, res) {
 
 });
 
-app.get('/', function (req, res) {
-	res.send('Hello World');
+app.get('/testSocket', function (req, res) {
+	res.sendfile('test.html');
+});*/
+
+
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname));
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+io.set('origins', '*:*');
+
+
+
+app.get('/', function(req, res){
+  res.sendfile('test.html');
 });
 
-var server = app.listen(8080, function () {
-	var host = server.address().address
-	var port = server.address().port
-})
+
+
+
+
+var nsp = io.of('/myLocation');
+nsp.on('connection', function(socket){
+  console.log('A user connected');
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
